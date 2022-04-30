8e479200853d9cef4127ef36ed61de84aa1ff985
Ramy <ramy@Kurama>
2022-04-30 18:30:46 -0400

    updated Interface imp

--- Timberwolf.java
+++ Timberwolf.java
@@ -1,2 +1,30 @@
 public class Timberwolf implements SimulatedAnnealing{
+
+    private double initTemp; 
+    
+
+      @Override
+    public void swap() {
+        
+    }
+
+    @Override
+    public void calcCost() {
+
+    }
+
+    @Override
+    public void comparePlacement() {
+
+    }
+
+    @Override
+    public void updateTemp() {
+
+    }
+
+    @Override
+    public void setInitTemp() {
+
+    }
 }
