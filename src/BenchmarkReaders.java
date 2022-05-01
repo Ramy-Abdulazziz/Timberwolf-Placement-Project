0aba39bd71724fc5d289bbf473b68b49c6d73cb5
Ramy-Abdulazziz <ramy.abdulazziz@gmail.com>
2022-04-30 23:31:12 -0400

    Node file reader

--- src/BenchmarkReaders.java
+++ src/BenchmarkReaders.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Iterator;
 
 public class BenchmarkReaders {
@@ -17,6 +18,7 @@ public class BenchmarkReaders {
             int lineCount = 0;
 
             while (lineReader.hasNext()) {
+
                 lineCount++;
 
                 if (lineCount == 6) {
@@ -28,16 +30,13 @@ public class BenchmarkReaders {
                     int numTerminals = Integer.parseInt(line[2]);
 
                 }
+
                 if (lineCount > 7) {
 
                     reader.lines().forEach(line -> {
 
-                        boolean terminal = false;
                         String[] nodeLine = line.split("\\s+");
-
-                        if (line.length() > 4 && nodeLine[4].equals("terminal")) {
-                             terminal = true;
-                        }
+                        boolean isTerminal = Arrays.stream(nodeLine).anyMatch(str -> str.equals("Terminal"));
 
                         String name = nodeLine[1];
                         int width = Integer.parseInt(nodeLine[2]);
@@ -46,6 +45,7 @@ public class BenchmarkReaders {
 
                 }
 
+                lineReader.next();
 
             }
         }catch(FileNotFoundException e){
